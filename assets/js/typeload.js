86eb9f651d77c36561661ca0129c1711db2e1e9e
Lewis Price <37052196+LewisP2018@users.noreply.github.com>
2018-03-05 04:42:36 +0000

    .

--- assets/js/typeload.js
+++ assets/js/typeload.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
     $("span.typed").typed({
-        strings: ["Free Server Hosting", "DDoS Protection", "No Queue", "And More"],
+        strings: ["Free Server Hosting", "DDoS Protection", "No Queue", "Welcome to Minehut"],
         typeSpeed: 50,
         backSpeed: 30,
         backDelay: 700,
