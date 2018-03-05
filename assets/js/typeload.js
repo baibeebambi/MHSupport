5c2fbec4a2aa345d4bc6092029e1b33a243556e8
Lewis Price <37052196+LewisP2018@users.noreply.github.com>
2018-03-05 02:47:12 +0000

    .

--- assets/js/typeload.js
+++ assets/js/typeload.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
     $("span.typed").typed({
-        strings: ["Free Server Hosting", "Free Server Hosting", "Free Server Hosting", "Free Server Hosting"],
+        strings: ["Free Server Hosting", "Minehut.com", "Free Server Hosting", "Minehut.com"],
         typeSpeed: 50,
         backSpeed: 30,
         backDelay: 700,
