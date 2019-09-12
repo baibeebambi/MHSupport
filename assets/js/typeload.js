5844ce9a3d75f531fb272a908b4448070ed4a456
baibeebambi <37086019+baibeebambi@users.noreply.github.com>
2019-09-12 17:30:42 -0400

    Update typeload.js

--- assets/js/typeload.js
+++ assets/js/typeload.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
     $("span.typed").typed({
-        strings: ["Answers now", "MHSupport"],
+        strings: ["We provide you with the answers you need", "Minehut Support"],
         typeSpeed: 50,
         backSpeed: 30,
         backDelay: 700,
